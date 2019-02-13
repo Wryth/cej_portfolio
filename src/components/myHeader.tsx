cdcf7e644846d59c0abc1f6b7824d32e72568aed
DESKTOP-834203O\Viktor <vikhan91@gmail.com>
2019-02-13 05:39:23 +0100

    Started working on the onMouseHover stuff, still needs to be able to change the fonts, and that the function is capable on all texts.

--- src/components/myHeader.tsx
+++ src/components/myHeader.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import './myHeader.css';
 
-
 class MyHeader extends React.Component {
     constructor(props:number) {
         super(props);
-        this.state = {isToggleOn: true};
-    
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
       }
     
-      handleClick() {
-        this.setState(state => ({
-          //isToggleOn: !state.isToggleOn
-        }));
-      }
+
+        private mouseOver = ():void => {
+            //this.style.color = "red";
+            console.log("Its Here!:", this);
+
+
+        }
+        
+        private mouseOut = ():void => {
+            console.log("It left!")
+            //document.getElementById("name").style.color = "black";
+        }
 
 
     render(){
         return(
             <div className="MyHeaderContainer">
-                        <h1 id="name">CARL EMIL JACOBSEN</h1>
+                        <h1 id="name"
+                        onMouseOver={this.mouseOver} 
+                        onMouseOut ={this.mouseOut}
+                        >CARL EMIL JACOBSEN
+                        </h1>
                         <div className="headerMenu">
                             <p className="headerOptions">X</p>
                             <p className="headerOptions">Instagram</p>
